bc9f8a07c44001435fb8a8c2d2a22aed4445c273
陈希瑞 <smartloe@qq.com>
2025-11-04 01:31:14 +0800

    Update run.py

--- tests/baidu_spider/run.py
+++ tests/baidu_spider/run.py
@@ -1,19 +1,15 @@
 import asyncio
-import sys
-import os
-import time
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from bald_spider.core.engine import Engine
 from tests.baidu_spider.baidu import BaiduSpider
+from bald_spider.utils.project import get_settings
 
 
 async def run():
-    baidu_spider = BaiduSpider()
-    engine = Engine()
-    await engine.start_spider(baidu_spider)
+    settings = get_settings()
+
+    # baidu_spider = BaiduSpider()
+    # engine = Engine()
+    # await engine.start_spider(baidu_spider)
 
 
 asyncio.run(run())
-
