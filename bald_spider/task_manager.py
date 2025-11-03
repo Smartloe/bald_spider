efa802ea560ed1c84b5c71ed066a3d87be92ef76
陈希瑞 <smartloe@qq.com>
2025-11-04 00:52:55 +0800

    Create task_manager.py

---
+++ bald_spider/task_manager.py
@@ -0,0 +1,20 @@
+from typing import Set, Final
+import asyncio
+from asyncio import Task, Future
+
+
+class TaskManager:
+    def __init__(self):
+        self.current_task: Final[Set] = set()
+
+    def create_task(self, coroutine) -> Task:
+        task = asyncio.create_task(coroutine)
+        self.current_task.add(task)
+
+        def done_callback(_fut: Future):
+            self.current_task.remove(task)
+
+        task.add_done_callback(done_callback)
+
+    def all_done(self):
+        return len(self.current_task) == 0
